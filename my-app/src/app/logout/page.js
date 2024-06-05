8130e78e2a7bc58ae207b80b97f4e996f584f2c5
shivam kumar <mamtadv390@gmail.com>
2024-06-05 22:20:45 +0530

    adding different pages for app

---
+++ my-app/src/app/logout/page.js
@@ -0,0 +1,11 @@
+import React from 'react'
+
+const signin = () => {
+  return (
+    <div>
+      you are sinsignin page
+    </div>
+  )
+}
+
+export default signin
---
+++ my-app/src/app/products/[slug]/page.js
@@ -0,0 +1,5 @@
+  function Product({params}) {
+  return <div>{params.slug}</div>
+
+}
+export default Product
